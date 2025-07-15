33298bcbd9c933daa003a829d4109624651763a8
k713shino <shino.techno@gmail.com>
2025-07-15 15:09:25 +0900

    プロフィール画像GitHub対応

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ module.exports = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
         port: '',
         pathname: '/**',
