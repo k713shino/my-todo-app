3ea7426d8d1f17f95a62e0fb041d2b08aef3dcd8
k713shino <shino.techno@gmail.com>
2025-07-17 09:49:33 +0900

    データベースのダミー参照を破棄2

--- next.config.ts
+++ next.config.ts
@@ -21,10 +21,10 @@ const nextConfig = {
   },
   
   // 環境変数の設定
-  env: {
+  //env: {
     // ビルド時のダミーDATA BASE_URL
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dummy:dummy@localhost:5432/dummy?connect_timeout=1',
-  },
+  //  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dummy:dummy@localhost:5432/dummy?connect_timeout=1',
+  //},
   
   // 画像設定
   images: {
